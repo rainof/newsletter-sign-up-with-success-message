f07cf792526d75f782a0acb4475717edc0cde84b
rainof <13416303+rainof@users.noreply.github.com>
2024-08-11 00:15:28 +0700

    chore: add subscription form component

--- src/components/SubscriptionForm.js
+++ src/components/SubscriptionForm.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import '../styles/SubscriptionForm.scss';
 
 function SubscriptionForm({formInfo}) {
     return (
-        <div></div>
+        <div className="form-container">
+            <p className="form-title">{formInfo.title}</p>
+            <input type="email" id="email" name="email" placeholder={formInfo.initial} required/>
+            <button id="submit-button">{formInfo.buttonName}</button>
+        </div>
     )
 }
 
