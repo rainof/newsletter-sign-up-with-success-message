e7234efe4d48cec4f7c5971c2821fb5b11107147
rainof <13416303+rainof@users.noreply.github.com>
2024-08-11 02:14:22 +0700

    fix: add email validation rule in subscription form

--- src/components/SubscriptionForm.js
+++ src/components/SubscriptionForm.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/SubscriptionForm.scss';
 
 function SubscriptionForm({formInfo}) {
+
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+        if (emailPattern.test(email)) {
+        setMessage(`Thanks for subscribing! A confirmation email has been sent to ${email}.`);
+        } else {
+        setMessage('Please enter a valid email address.');
+        }
+    };
+
+
     return (
         <div className="form-container">
-            <p className="form-title">{formInfo.title}</p>
-            <input type="email" id="email" name="email" placeholder={formInfo.initial} required/>
-            <button id="submit-button">{formInfo.buttonName}</button>
+            <form onSubmit={handleSubmit}>
+                <p className="form-title">{formInfo.title}</p>
+                <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder={formInfo.initial}
+                    required
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <button type="submit" id="submit-button">
+                    {formInfo.buttonName}
+                </button>
+            </form>
         </div>
-    )
+    );
 }
 
 export default SubscriptionForm;
