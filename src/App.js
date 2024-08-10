bf596e5d1cbb452dd2c7c3cd0e802612d7583d14
rainof <13416303+rainof@users.noreply.github.com>
2024-08-10 14:48:19 +0700

    chore: structure project files and configure initial settings and information

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import HeroImage from 'src/components/HeroImage.js'
+import MainContent from 'src/components/MainContent.js'
+import SubscriptionForm from 'src/components/SubscriptionForm.js'
 
-function App() {
+function App({ contentInfo, formInfo, successPageInfo }) {
   return (
-    <div></div>
+    <div className="App">
+      <HeroImage value={contentInfo} />
+      <MainContent value={formInfo} />
+      <SubscriptionForm value={successPageInfo} />
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -5,9 +5,32 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const contentInfo = {
+  title: "Stay updated!",
+  description: "Join 60,000+ product managers receiving monthly updates on:",
+  points: [
+    "Product discovery and building what matters",
+    "Measuring to ensure updates are a success",
+    "And much more!"
+  ],
+};
+
+const formInfo = {
+  title: "Email address",
+  initial: "email@company.com",
+  buttonName: "Subscribe to monthly newsletter"
+};
+
+const successPageInfo = {
+  title: "Thanks for subscribing!",
+  description: "A confirmation email has been sent to ash@loremcompany.com. Please open it and click the button inside to confirm your subscription.",
+  buttonName: "Dismiss message"
+};
+
 root.render(
   <React.StrictMode>
-    <App />
+    <App contentInfo={contentInfo} formInfo={formInfo} successPageInfo={successPageInfo} />
   </React.StrictMode>
 );
 
