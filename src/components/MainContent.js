e9eb3b8f38d92058a1281ed27f276229f8002cfd
rainof <13416303+rainof@users.noreply.github.com>
2024-08-10 22:16:45 +0700

    chore: add content component

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import '../styles/MainContent.scss';
 
 function MainContent({contentInfo}) {
     return (
-        <div>
+        <div className="content-container">
+            <h1 className="content-title">{contentInfo.title}</h1>
+            <p className="content-description">{contentInfo.description}</p>
+            <ul className="custom-list">
+                {contentInfo.points.map((point, i) => (
+                    <li key={"point_"+i} className="content-bullet">{point}</li>
+                ))}
+            </ul>
         </div>
     )
 }
