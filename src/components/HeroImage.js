ab04023b9d7817c97b9da895c1f5d249c0881c02
rainof <13416303+rainof@users.noreply.github.com>
2024-08-11 23:17:33 +0700

    fix: correct image display logic based on screen size

--- src/components/HeroImage.js
+++ src/components/HeroImage.js
@@ -4,11 +4,15 @@ import heroMobileImage from '../assets/images/illustration-sign-up-mobile.svg';
 import heroDesktopImage from '../assets/images/illustration-sign-up-desktop.svg';
 
 function HeroImage() {
-    const isMobile = window.innerWidth < 768;
+    // const isMobile = window.innerWidth < 900;
 
     return (
         <div className="hero-image-container">
-            <img id="background-image" src={ isMobile ? heroMobileImage : heroDesktopImage} alt="Hero" />
+            <picture>
+                <source media="(max-width: 899px)" srcset={heroMobileImage}></source>
+                <source media="(min-width: 900px)" srcset={heroDesktopImage}></source>
+                <img src={heroDesktopImage} alt="Hero" />
+            </picture>
         </div>
     );
 }
